6c547549bc61696b4e58f2aaa4ed6a9c1853d566
Diego Ventura <dventurasilva@wayfair.com>
2022-06-13 20:36:03 +0200

    Add script to dump all available cars

---
+++ Scripts/bootstrap.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function download_all() {
+    # Hide cursor
+    tput civis 
+
+    parameters=("$@")
+    folder=${parameters[0]}
+    
+    unset parameters[0]
+    totalCars=431
+    
+    [ -d $folder ] && echo "Directory Exists" || mkdir -p $folder
+
+    cd $folder
+
+    echo "Downloading GT7 cars..."
+    
+    for (( index=0; index<$totalCars; index++ )); do
+        currentCar=$(( $index+1 ))
+        percentage=$(echo "scale=0; ($currentCar * 100) / $totalCars" | bc -l)
+        carId=$(printf "%03d" $currentCar)
+        filename="car_$carId.html"
+
+        echo "[$carId/$totalCars - Progress $percentage%] - Filename: $filename\r"
+        download_car $index $filename
+    done
+
+    # Set cursor back
+    tput cnorm 
+    
+    echo "\n"
+    echo "Done!"
+}
+
+function download_car() {
+    carId=$(( $1+1 ))
+    filename=$2
+
+    ## Request
+    curl "https://www.kudosprime.com/gt7/car_sheet.php?id=$carId" \
+    -H 'Cookie: PHPSESSID=jae0ovjkm7ca0aturb808opc51' \
+    --output $filename \
+    --silent
+}
+
+function main() {
+    local folder="./data/html"
+    download_all $folder
+}
+
+main
