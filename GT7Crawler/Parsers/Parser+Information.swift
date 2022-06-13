82ac7f4d0d177948cd341a1c9d7a799e93954eee
Diego Ventura <dventurasilva@wayfair.com>
2022-06-13 19:12:52 +0200

    Add car identifier on information parser

--- GT7Crawler/Parsers/Parser+Information.swift
+++ GT7Crawler/Parsers/Parser+Information.swift
@@ -8,11 +8,12 @@
 import Foundation
 
 extension Parser {
-    func parseInformation() -> (carName: String, manufacturer: String) {
+    func parseInformation() -> (identifier: String, carName: String, manufacturer: String) {
         let element = try? document.getElementsByClass("title").first()
         let manufacturer = (try? element?.getElementsByTag("p").text()) ?? ""
         let carName = (try? element?.getElementsByTag("h1").text()) ?? ""
+        let identifier = (try? document.getElementsByClass("main_img").first()?.attr("data-carid")) ?? "No ID"
         
-        return (carName: carName, manufacturer: manufacturer)
+        return (identifier: identifier, carName: carName, manufacturer: manufacturer)
     }
 }
